7cf8f7e127bab2f638855a6885bdb87ae6c71cbf
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-04-14 12:06:40 +0200

    dev compilable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListePatientsComponent } from './patient/liste-patients/liste-patients.component';
-import { AjouterPatientComponent } from './ajouter-patient/ajouter-patient.component';
-import { ModifierPatientComponent } from './modifier-patient/modifier-patient.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListePatientsComponent,
-    AjouterPatientComponent,
-    ModifierPatientComponent
   ],
   imports: [
     BrowserModule
